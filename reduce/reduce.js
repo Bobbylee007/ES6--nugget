138087efbdbc940e3aa227c4f02d6fabcace1803
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-12-04 10:29:33 +0100

    reducer intro

---
+++ reduce/reduce.js
@@ -0,0 +1,13 @@
+// Reduce
+// iterates, callback function
+// reduce to a single value - number, array, object
+// 1st parameter ('acc') - total of all calculates
+// 2nd parameter ('curr') - current iteration/avlue
+
+
+const people = [
+  { name: "bob", age: 20, position: "developer" },
+  { name: "peter", age: 25, position: "designer" },
+  { name: "susy", age: 30, position: "the boss" },
+  { name: "anna", age: 35, position: "intern" },
+];
