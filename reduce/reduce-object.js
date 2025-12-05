3528bfb8591304b9c011ad6f4738f4675766b919
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-12-05 11:34:25 +0100

    reducer basics

---
+++ reduce/reduce-object.js
@@ -0,0 +1,10 @@
+// Reduce - Object
+
+// cart example
+const cart = [
+
+]
+
+// github repos Example
+
+const url = 'https://api.github.com/users/john-smilga/repos?per_page=100'
--- reduce/reduce.js
+++ reduce/reduce.js
@@ -4,10 +4,18 @@
 // 1st parameter ('acc') - total of all calculates
 // 2nd parameter ('curr') - current iteration/avlue
 
-
-const people = [
-  { name: "bob", age: 20, position: "developer" },
-  { name: "peter", age: 25, position: "designer" },
-  { name: "susy", age: 30, position: "the boss" },
-  { name: "anna", age: 35, position: "intern" },
+const staff = [
+  { name: "bob", age: 20, position: "developer", salary: 100 },
+  { name: "peter", age: 25, position: "designer", salary: 300 },
+  { name: "susy", age: 30, position: "the boss", salary: 400 },
+  { name: "anna", age: 35, position: "intern", salary: 10 },
 ];
+
+const dailyTotal = staff.reduce((total, person) => {
+  console.log(total);
+  console.log(person.salary);
+  total += person.salary
+  return total;
+},0);
+
+console.log(dailyTotal);
