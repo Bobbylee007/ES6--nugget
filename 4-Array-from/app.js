702923298d9079a5eb882b286533be353f8053d3
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-12-12 09:14:57 +0100

    Array.from example 2

--- 4-Array-from/app.js
+++ 4-Array-from/app.js
@@ -15,4 +15,20 @@ const text = document.querySelectorAll(".text");
 //console.log(newText);  it been converted to array
 
 const newText = Array.from(text).find((item) => item.textContent === "Person");
-console.log(newText);
+// console.log(newText);
+
+// General setting For pagination
+const items = Array.from({ length: 120 }, (_, index) => {
+  return index;
+});
+
+const itemsPerPage = 8;
+const pages = Math.ceil(items.length / itemsPerPage);
+
+const newItems = Array.from({ length: pages }, (_, index) => {
+  const start = index * itemsPerPage;
+  const tempItems = items.slice(start, start + itemsPerPage)
+  return tempItems
+});
+
+console.log(newItems);
