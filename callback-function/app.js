291de9eb5b18b286d81f41499d0f7a45da5dcf26
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-12-31 19:46:38 +0100

    callback-hell and example

--- callback-function/app.js
+++ callback-function/app.js
@@ -5,31 +5,32 @@ function makeUppercase(value) {
 // makeUppercase('peter');
 
 function reverseString(value) {
-    console.log(value.split('').reverse().join(''));
-    
+  console.log(value.split("").reverse().join(""));
 }
 
-function handleName(name, cb){
-    const fullName = `${name} smith`
-    cb(fullName)
-    // some more logic
-    
+function handleName(name, cb) {
+  const fullName = `${name} smith`;
+  cb(fullName);
+  // some more logic
 }
-
 
 // handleName('peter', makeUppercase)
 // handleName('peter', reverseString);
 
-//As a arrow func
-// handleName('susan', function (value){
+// As a arrow funct
+//  handleName('susan', function (value){
 //     console.log(value);
-    
+
 // })
 
-// As a arrow func
-handleName('susan', function (value){
-    console.log(value);
+// As a liner
+handleName("susan", (value) => console.log(value));
+
+
+const btn = document.querySelector('.btn')
+btn.addEventListener('click', function () {
+    console.log('hello world');
+    
     
 })
-
 // array method, setTimeout, event listeners etc
