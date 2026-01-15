4dc06c791751a469feefbe8e164a5a70d9baf925
Bobbylee007 <bobbyleeaustine@gmail.com>
2026-01-15 10:16:55 +0100

    fetch basics

--- 17-fectch/app.js
+++ 17-fectch/app.js
@@ -6,6 +6,21 @@
 const url = "https://www.course-api.com/react-tour-project";
 
 // console.log(fetch(url));
-fetch(url)
-  .then((resp) => console.log())
-  .catch((err) => console.log(err));
+// fetch(url)
+//   .then((resp) => resp.json())
+//   .then((data)=> console.log(data))
+//   .catch((err) => console.log(err));
+
+
+const getTours = async () =>{
+  try {
+    const resp = await fetch(url)
+    const data = await resp.json()
+    // console.log(data);
+    
+  } catch (error){
+    console.log(error); 
+  }
+}
+console.log(getTours().then());
+
