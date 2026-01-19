24c23c1208dc9375404f4a8c4335aef51cf12950
Bobbylee007 <bobbyleeaustine@gmail.com>
2026-01-19 10:44:52 +0100

    fetch-error and example

---
+++ 18-fectch-errors/app.js
@@ -0,0 +1,25 @@
+// Javascript Nuggets
+// Fetch Error 
+// Only throws an error if cannot resolve. 
+// Error Response srill a response (400-500)
+
+const url = 'https://www.course-api.com/react-tours-projec'
+ const getTours = async ( ) => {
+    try {
+        const resp =await fetch(url)
+        if (!resp.ok) {
+            const msg=` There was an error"${resp.status} ${resp.statusText}" `
+            throw new Error(msg);
+            
+        }
+       
+        const tours = await resp.json()
+        console.log(tours)
+    } catch (error) {
+        console.log(error);
+        
+    }
+ }
+
+ const btn = document.querySelector('.btn')
+ btn.addEventListener('click', getTours)
