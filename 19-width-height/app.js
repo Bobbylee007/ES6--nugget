93f5a45be32a27ecdcfa716db75b29e83647f419
Bobbylee007 <bobbyleeaustine@gmail.com>
2026-01-28 12:25:13 +0100

    Bounding Elents

---
+++ 19-width-height/app.js
@@ -0,0 +1,18 @@
+// Javascript Nuggets
+// Width/Height - window, any element 
+// innerHeight- window 
+// innerWidth - window
+// getBoundingClientReact() - any element
+
+
+console.log("height", window.innerHeight);
+console.log("width", window.innerWidth);
+
+const btn = document.querySelector(".btn");
+const box = document.querySelector('.box')
+
+btn.addEventListener('click', ()=>{
+    const dimensions = box.getBoundingClientRect()
+    console.log(dimensions);
+    
+})
