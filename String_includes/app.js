a68e40a72b1afc230b744a289991950ddded7cf8
Bobbylee007 <bobbyleeaustine@gmail.com>
2026-02-04 10:11:46 +0100

    String includes and Example

---
+++ String_includes/app.js
@@ -0,0 +1,30 @@
+// String includes() - checks if a string contains another string.
+
+const products = [
+  { title: "high-back bench", company: "ikea" },
+  { title: "albany table", company: "marcos" },
+  { title: "accent chair", company: "caressa" },
+  { title: "wooden table", company: "ikea" },
+];
+
+
+
+//===============tech=================
+// const firstName = 'Bobby'
+// // const result = firstName.includes('b',2)
+
+
+// const product = {
+//     title:"Leather Chair",
+// }
+
+
+// const result = product.title.includes('Leather')
+// console.log(result);
+
+
+// ============function that will get text and check if styles inclues the text==========
+const text = 'a'
+
+const filteredProducts = products.filter((product)=> product.title.toLowerCase().includes(text))
+console.log(filteredProducts);
